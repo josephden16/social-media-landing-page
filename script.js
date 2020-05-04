4f07a22f121df7a115cf507770a99dfc69c5825f
josephden16 <tegajoseph2002@gmail.com>
2020-05-04 12:03:25 +0100

    made toggle btn

---
+++ script.js
@@ -0,0 +1,22 @@
+let btn = document.querySelector(".btn");
+let slider = document.querySelector(".slider");
+btn.addEventListener("click", move);
+let currentPos = "left";
+function move() {
+    switch (currentPos) {
+        case "left":
+            btn.style.backgroundImage =  "linear-gradient(to right,  hsl(210, 78%, 56%),hsl(146, 68%, 55%))";
+            slider.style.backgroundColor = "hsl(230, 17%, 14%)";
+            btn.style.justifyContent = "flex-end";
+            body.style.backgroundColor = "hsl(230, 17%, 14%)";
+            currentPos = "right";
+            break;
+        case "right":
+            btn.style.backgroundImage = "none";
+            slider.style.backgroundColor = "#fff";
+            btn.style.justifyContent = "flex-start";
+            body.style.backgroundColor = "#fff";
+            currentPos = "left"
+            break;
+    }
+}
