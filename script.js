d060143f6e13e90b75ede9743dff57a0c917ccdb
josephden16 <tegajoseph2002@gmail.com>
2020-05-05 11:02:03 +0100

    worked on grid overview cards

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let btn = document.querySelector(".btn");
 let slider = document.querySelector(".slider");
+let body = document.querySelector(".body");
 btn.addEventListener("click", move);
 let currentPos = "left";
 function move() {
     switch (currentPos) {
         case "left":
-            btn.style.backgroundImage =  "linear-gradient(to right,  hsl(210, 78%, 56%),hsl(146, 68%, 55%))";
+            btn.style.backgroundImage =  "linear-gradient(to right,  hsl(210, 78%, 56%), hsl(146, 68%, 55%))";
             slider.style.backgroundColor = "hsl(230, 17%, 14%)";
             btn.style.justifyContent = "flex-end";
             body.style.backgroundColor = "hsl(230, 17%, 14%)";
