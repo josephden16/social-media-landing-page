c5e6e454c00c3a3dba1aee52997a4e77d01c98e8
josephden16 <tegajoseph2002@gmail.com>
2020-05-05 15:51:10 +0100

    fixed dashboard

--- script.js
+++ script.js
@@ -9,14 +9,12 @@ function move() {
             btn.style.backgroundImage =  "linear-gradient(to right,  hsl(210, 78%, 56%), hsl(146, 68%, 55%))";
             slider.style.backgroundColor = "hsl(230, 17%, 14%)";
             btn.style.justifyContent = "flex-end";
-            body.style.backgroundColor = "hsl(230, 17%, 14%)";
             currentPos = "right";
             break;
         case "right":
             btn.style.backgroundImage = "none";
             slider.style.backgroundColor = "#fff";
             btn.style.justifyContent = "flex-start";
-            body.style.backgroundColor = "#fff";
             currentPos = "left"
             break;
     }
